941402a6a1fa7ada7214606f390637f78c0427f3
Luis Escamilla <luis.escamilla@correounivalle.edu.co>
2019-10-03 18:55:20 -0500

    Chart and Note ready

--- Sticky/urls.py
+++ Sticky/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from django.urls import path
 from django.contrib import admin
 from user.models import User
+from chart.models import Chart
+from note.models import Note
 from rest_framework import routers, serializers, viewsets
 from django import forms
 
@@ -10,15 +12,32 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+class ChartSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Chart
+        fields = '__all__'
+class NoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class ChartViewSet(viewsets.ModelViewSet):
+    queryset = Chart.objects.all()
+    serializer_class = ChartSerializer
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'charts', ChartViewSet)
+router.register(r'note', NoteViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- chart/models.py
+++ chart/models.py
@@ -3,7 +3,7 @@ from user.models import User
 
 # Create your models here.
 class Chart(models.Model):
-	author	= models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=True)
+	owner	= models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=True)
 	public 	= models.BooleanField(default=True)
 	title	= models.CharField(max_length=250, blank=False, null=True)
 	
