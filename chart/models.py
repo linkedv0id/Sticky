59f8e26cd260cf4ccadf3197459cb8762c9ea7a9
Luis Escamilla <luis.escamilla@correounivalle.edu.co>
2019-10-03 19:59:48 -0500

    Fixed chart model 'privacy' field

--- chart/models.py
+++ chart/models.py
@@ -3,8 +3,8 @@ from user.models import User
 
 # Create your models here.
 class Chart(models.Model):
-	owner	= models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=True)
-	public 	= models.BooleanField(default=True)
-	title	= models.CharField(max_length=250, blank=False, null=True)
+	owner	 = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=True)
+	privacy	 = models.BooleanField(default=True)
+	title	 = models.CharField(max_length=250, blank=False, null=True)
 	
 
